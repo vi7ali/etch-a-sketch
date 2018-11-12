a50d3fdd8efaa19c8672cb1d12e53bdcb0722a6c
Vitaly Osipov <vi7ali@gmail.com>
2018-11-12 17:18:55 +0300

    Layout changed to grid, added 2 modes for color change, darker-color mode now works as intended

--- js/script.js
+++ js/script.js
@@ -1,53 +1,12 @@
 const container = document.querySelector("#container");
 const newButton = document.querySelector("#bnew");
-const clearButton = document.querySelector("#clear");
+const randomColorOption = document.querySelector("#random-color");
 const containerPosition = container.getBoundingClientRect();
 const sideMax = containerPosition.width;
 
 createGrid(16);
 
 newButton.addEventListener("click", getNewGrid);
-clearButton.addEventListener("click", clearGrid);
-
-function createGrid(cellsTotal) {
-	clearGrid();
-	for (i=0; i<cellsTotal*cellsTotal; i++) {
-		let cell = document.createElement("div");
-		cell.addEventListener("mouseover", changeColor)
-		cell.classList.add("cell");
-		cell.style.width = Math.round((sideMax / cellsTotal)*100) / 100 + "px";
-		cell.style.height = Math.round((sideMax / cellsTotal)*100) / 100 + "px";
-		container.appendChild(cell);
-	}	
-}
-
-function clearGrid() {
-	let allCells = document.querySelectorAll(".cell");
-	allCells.forEach((cell) => cell.remove());
-}
-
-function changeColor(e) {
-	let currentCell = e.target;
-	if(currentCell.classList.contains("painted")) {
-		currentCell.style.backgroundColor = addBlack(currentCell);
-	}
-	else {
-	 currentCell.classList.add("painted");
-	 currentCell.style.backgroundColor = randomPaint();
-	}
-}
-
-function addBlack(cell) {
-
-	let currentColor = cell.style.backgroundColor;
-	let newColor = currentColor.slice(4, currentColor.length-1);
-	newColor = newColor.split(",");
-
-	for(i=0;i<newColor.length;i++) {
-		newColor[i] = newColor[i] * 0.90;
-	}
-	return `rgb(${newColor.join()})`;
-}
 
 function getNewGrid() {
 	var uInput = prompt("Please enter the number of cells between 1 and 64");
@@ -60,15 +19,56 @@ function getNewGrid() {
 		alert("Invalid number. It must be between 1 and 64");
 		return;
 	}
-	else {
-		createGrid(uInput);
+	else createGrid(uInput);
+}
+
+function createGrid(cellsTotal) {
+	clearGrid();
+	container.style.gridTemplate = `repeat(${cellsTotal}, 1fr) / repeat(${cellsTotal}, 1fr)`;
+	for (i=0; i<cellsTotal*cellsTotal; i++) {
+		let cell = document.createElement("div");
+		cell.classList.add("cell");
+		cell.setAttribute("data-passes", "0"); // This attribute will help with the calculations to make the cell totally black after exactly 10 passes
+		cell.addEventListener("mouseover", changeColor);
+		container.appendChild(cell);
+	}	
+}
+
+function clearGrid() {
+	let allCells = document.querySelectorAll(".cell");
+	allCells.forEach((cell) => cell.remove());
+}
+
+function changeColor(e) {
+	let cell = e.target;
+	let currentColor = cell.style.backgroundColor;	
+	let numberOfPasses = cell.getAttribute("data-passes");
+	let color = randomPaint();
+
+	if(numberOfPasses>0 && !randomColorOption.checked) {
+		let darkerColor = currentColor.slice(4, currentColor.length-1);
+		darkerColor = darkerColor.split(",");
+
+		for(i=0;i<darkerColor.length;i++) {
+			let originalPercent = ((darkerColor[i] * 100)/(100 - numberOfPasses * 10))*0.1; // 10% of the original value using reverse percentage formula
+			darkerColor[i] = darkerColor[i] - originalPercent;
+		}
+		cell.style.backgroundColor = `rgb(${darkerColor.join()})`;
 	}
+	else cell.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;	
+
+	if(!randomColorOption.checked && numberOfPasses<10)	{
+		numberOfPasses++;
+		cell.setAttribute("data-passes", numberOfPasses);
+	}
+	
+	if(randomColorOption.checked) cell.setAttribute("data-passes", 1);	
 }
 
 function randomPaint() {
-	let r = Math.floor(Math.random()*256);
-	let g = Math.floor(Math.random()*256);
-	let b = Math.floor(Math.random()*256);
-	
-	return `rgb(${r}, ${g}, ${b})`;
+	let color = [];
+	for (i=0; i<3;i++) {
+		color[i] = Math.floor(Math.random()*256);	
+	}	
+	return color;
 }
