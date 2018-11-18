71861f18a54b3be8eb9198840ded7cc09d19b78b
Vitaly Osipov <vi7ali@gmail.com>
2018-11-18 19:45:11 +0300

    On branch refracture Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js
    Added black color option

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
 const newButton = document.querySelector("#bnew");
 const randomColorOption = document.querySelector("#random-color");
+const darkerColorOption = document.querySelector("#darker-color");
+const blackColorOption = document.querySelector("#black-color");
 const containerPosition = container.getBoundingClientRect();
 const sideMax = containerPosition.width;
 
@@ -39,36 +41,41 @@ function clearGrid() {
 	allCells.forEach((cell) => cell.remove());
 }
 
-function changeColor(e) {
+function changeColor(e) 
+{
 	let cell = e.target;
-	let currentColor = cell.style.backgroundColor;	
+	let currentColor = cell.style.backgroundColor;
 	let numberOfPasses = cell.getAttribute("data-passes");
 	let color = randomPaint();
 
-	if(numberOfPasses>0 && !randomColorOption.checked) {
+	if(blackColorOption.checked) {
+		cell.style.backgroundColor = `rgb(0, 0, 0)`;
+		cell.setAttribute("data-passes", "1")
+	}
+	else if(randomColorOption.checked || (darkerColorOption.checked && numberOfPasses==0)) {
+		cell.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+		cell.setAttribute("data-passes", "1");
+	}
+	else if(darkerColorOption.checked) {
 		let darkerColor = currentColor.slice(4, currentColor.length-1);
 		darkerColor = darkerColor.split(",");
 
-		for(i=0;i<darkerColor.length;i++) {
-			let originalPercent = ((darkerColor[i] * 100)/(100 - numberOfPasses * 10))*0.1; // 10% of the original value using reverse percentage formula
+		for(i=0;i<darkerColor.length;i++)	{
+			let originalPercent = ((darkerColor[i] * 100)/(100 - (numberOfPasses-1) * 10))*0.1; // 10% of the original value using reverse percentage formula
 			darkerColor[i] = darkerColor[i] - originalPercent;
 		}
-		cell.style.backgroundColor = `rgb(${darkerColor.join()})`;
+		cell.style.backgroundColor = `rgb(${darkerColor.join()})`;	
+		if(numberOfPasses<10) {
+			numberOfPasses++;
+			cell.setAttribute("data-passes", numberOfPasses);
+		}
 	}
-	else cell.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;	
-
-	if(!randomColorOption.checked && numberOfPasses<10)	{
-		numberOfPasses++;
-		cell.setAttribute("data-passes", numberOfPasses);
-	}
-	
-	if(randomColorOption.checked) cell.setAttribute("data-passes", 1);	
 }
 
 function randomPaint() {
 	let color = [];
 	for (i=0; i<3;i++) {
 		color[i] = Math.floor(Math.random()*256);	
-	}	
+	}
 	return color;
 }
